25d54519d34d88519ffe5ea4e4dc4d1203b3d1da
troi <blametroi@gmail.com>
2024-10-23 07:18:11 -0400

    once more dear friends

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -4,24 +4,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cmake-project-default-build-dir-name "build/")
- '(comment-empty-lines t)
- '(comment-style 'box-multi)
- '(confirm-kill-emacs 'y-or-n-p)
  '(package-selected-packages
-   '(ace-window astyle beacon cmake-mode cmake-project cobol-mode deft
-		diminish dot-mode dumb-jump exec-path-from-shell
-		flymake-guile flymake-ruby form-feed-st free-keys
-		geiser-guile gnuplot gnuplot-mode imenu-list inf-ruby
-		json-mode magit-file-icons magit-filenotify
-		magit-todos marginalia markdown-ts-mode
-		nerd-icons-completion nerd-icons-corfu
-		nerd-icons-dired nerd-icons-ibuffer
+   '(ace-window astyle beacon cmake-mode cobol-mode deft diminish
+		dot-mode dumb-jump exec-path-from-shell flymake-guile
+		form-feed-st free-keys geiser-guile gnuplot
+		gnuplot-mode imenu-list inf-ruby json-mode
+		magit-file-icons magit-filenotify magit-todos
+		marginalia markdown-ts-mode nerd-icons-completion
+		nerd-icons-corfu nerd-icons-dired nerd-icons-ibuffer
 		nerd-icons-ivy-rich ninja-mode show-font side-notes
 		smartparens tldr tomorrow-night-deepblue-theme
 		treesit-auto vertico wgrep-ag which-key ws-butler
-		yaml-mode ztree))
- '(standard-indent 8))
+		yaml-mode ztree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
