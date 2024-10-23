755bd5f8faa856670a84ee68a3e522902413523d
troi <blametroi@gmail.com>
2024-10-23 10:20:30 -0400

    pass 3

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -16,17 +16,20 @@
 ;; pass 1, reduce to a minimum usable init.
 ;; pass 2, removing use-package emacs, i don't like splitting the
 ;;         customizations away from related mode settings.
+;; pass 3, regroup and delete a lot of ideas to try comments.
 
 ;;;
 ;;; Code:
 ;;;
+
 
+
 ;;;
 ;;; system compatability checks
 ;;;
-;; system compatability checks: i don't move my init around to foreign
-;; systems that don't have current emacs builds. and i always intend
-;; to be running with the gui.
+
+;; i don't move my init around to foreign systems that don't have
+;; current emacs builds, and i always run with the gui.
 
 (when (< emacs-major-version 29)
   (error "This configuration requires Emacs 29 or newer!"))
@@ -34,7 +37,9 @@
 (when (not (display-graphic-p))
   (message "This configuration assumes you are running a GUI Emacs, some things may break.")
   (sleep-for 5))
+
 
+
 ;;;
 ;;; use-package and (m)elpa
 ;;;
@@ -60,244 +65,279 @@
           '(("gnu" . 10)
             ("nongnu" . 9)
             ("melpa-stable" . 6)
-            ("melpa" . 5)))
+            ("melpa" . 5))))
 
-  ;; sometimes a build of emacs doesn't grab the environment correctly,
-  ;; especially when run from a shortcut on macos. make sure the
-  ;; variables that i count on are set to match my shell login.
+;; sometimes a build of emacs doesn't grab the environment correctly,
+;; especially when run from a shortcut on macos. make sure the
+;; variables that i count on are set to match my shell login.
 
-  (use-package exec-path-from-shell
-    :config
-    (declare-function
-     exec-path-from-shell-initialize "exec-path-from-shell" ())
-    (declare-function
-     exec-path-from-shell-copy-envs "exec-path-from-shell")
+(use-package exec-path-from-shell
+  :config
+  (declare-function
+   exec-path-from-shell-initialize "exec-path-from-shell" ())
+  (declare-function
+   exec-path-from-shell-copy-envs "exec-path-from-shell")
 
-    (exec-path-from-shell-initialize)
-    (exec-path-from-shell-copy-envs
-     '("LIBRARY_PATH"
-       "INFOPATH"
-       "CPATH"
-       "MANPATH"
-       "CDPATH")))
-  
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs
+   '("LIBRARY_PATH"
+     "INFOPATH"
+     "CPATH"
+     "MANPATH"
+     "CDPATH")))
+
+
+
 ;;;
 ;;; sane defaults and establish some paths.
 ;;;
 
-  (add-to-list
-   'load-path
-   (concat user-emacs-directory "troi-lisp"))
+(add-to-list
+ 'load-path
+ (concat user-emacs-directory "troi-lisp"))
 
-  (if (file-exists-p
-       (concat user-emacs-directory
-	       "initial-scratch-message.txt"))
-      (with-current-buffer (get-buffer "*scratch*")
-        (delete-region (point-min) (point-max))
-	(insert-file-contents (concat user-emacs-directory
-				      "initial-scratch-message.txt"))
-	(set-buffer-modified-p nil))
-    (setopt initial-scratch-message ";; nothing to see here, move along"))
+(if (file-exists-p
+     (concat user-emacs-directory
+	     "initial-scratch-message.txt"))
+    (with-current-buffer (get-buffer "*scratch*")
+      (delete-region (point-min) (point-max))
+      (insert-file-contents (concat user-emacs-directory
+				    "initial-scratch-message.txt"))
+      (set-buffer-modified-p nil))
+  (setopt initial-scratch-message ";; nothing to see here, move along"))
 
-  ;; this makes TAB in the minibuffer behave more like it does in a
-  ;; shell.
-  (keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete)
+;;;
+;;; visuals
+;;;
 
-  (when (display-graphic-p)
-    (context-menu-mode))
+(when (display-graphic-p)
+  (context-menu-mode))
 
-  (when scroll-bar-mode
-    (scroll-bar-mode -1))
+(when scroll-bar-mode
+  (scroll-bar-mode -1))
 
-  (column-number-mode)
-  (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(column-number-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(setopt display-line-numbers-width 4)
+(setopt mode-line-position-column-line-format '(" (%l,%C)")) ; %C based 1, %c based 0
 
-  (add-hook 'prog-mode-hook 'which-function-mode)
+(add-hook 'prog-mode-hook 'which-function-mode)
 
-  (let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
-    (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
+(let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
+  (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
 
-  ;; turn on useful but disabled commands.
-  (put 'scroll-left 'disabled nil)
-  (put 'narrow-to-region 'disabled nil)
-  (put 'narrow-to-page 'disabled nil)
-  (put 'narrow-to-defun 'disabled nil)
-  (put 'upcase-region 'disabled nil)
-  (put 'downcase-region 'disabled nil)
+;; visual line mode is ok for text, use (truncate-lines t) for code.
 
-  ;; visual line mode is ok for text, use (truncate-lines t) for code.
-  (add-hook 'text-mode-hook 'visual-line-mode)
+(add-hook 'text-mode-hook 'visual-line-mode)
 
-  (delete-selection-mode +1)
-  (indent-tabs-mode +1)
+;; stragglers
 
-  ;; use s-q to close emacs if i don't want to M-x
-  ;; save-buffers-kill-emacs.
-  (global-unset-key (kbd "C-x C-c"))
+(setopt ns-auto-hide-menu-bar t)           ; this gains no space on displays with the notch
+(setopt use-dialog-box nil)
+(setopt apropos-sort-by-scores t)
+(setopt blink-matching-delay 0.1)
+(setopt standard-indent 8)
 
-  ;; the number of times i want a the list instead of the mode is zero.
-  (global-set-key (kbd "C-x C-d") 'dired)
-  (global-set-key (kbd "C-x C-b") 'ibuffer)
+;;;
+;;; metadata
+;;;
 
-  (global-set-key (kbd "M-o") 'other-window)
+(setopt user-full-name "Troy Brumley")
+(setopt user-mail-address "BlameTroi@gmail.com")
+(setopt auth-sources '("~/.authinfo.gpg"))
+(setopt auth-source-cache-expiry nil)
 
-  (global-set-key "\M-z" 'zap-up-to-char)
+;;;
+;;; nice scrolling
+;;;
 
-  ;; default search to regexp instead of string.
-  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(setopt scroll-margin 0)
+(setopt scroll-conservatively 100000)
+(setopt scroll-preserve-screen-position 1)
 
-  (setopt user-full-name "Troy Brumley")
-  (setopt user-mail-address "BlameTroi@gmail.com")
-  (setopt auth-sources '("~/.authinfo.gpg"))
-  (setopt auth-source-cache-expiry nil)
+;;;
+;;; miscellany
+;;;
 
-  ;; for line and column number modes, format with %C bases columns
-  ;; from 1 and not 0.
-  (setopt display-line-numbers-width 4)
-  (setopt mode-line-position-column-line-format '(" (%l,%C)"))
+(setopt sentence-end-double-space nil)
+(setopt require-final-newline t)
 
-  ;; nice scrolling
-  (setopt scroll-margin 0)
-  (setopt scroll-conservatively 100000)
-  (setopt scroll-preserve-screen-position 1)
+(setopt switch-to-buffer-obey-display-actions t)
+(setopt help-window-select t)
+(setopt help-wndow-keep-selected t)
+(setopt enable-recursive-minibuffers t)
 
-  (setopt sentence-end-double-space nil)
-  (setopt require-final-newline t)
+(setopt delete-by-moving-to-trash t)
 
-  (setopt switch-to-buffer-obey-display-actions t)
-  (setopt help-window-select t)
-  (setopt help-wndow-keep-selected t)
-  (setopt enable-recursive-minibuffers t)
+(delete-selection-mode +1)
+(indent-tabs-mode +1)
 
-  (setopt use-dialog-box nil)
-  (setopt apropos-sort-by-scores t)
-  (setopt blink-matching-delay 0.1)
-  (setopt delete-by-moving-to-trash t)
+(setopt tab-always-indent 'complete)
 
-  (setopt tab-always-indent 'complete)
+(setopt comment-empty-lines t)
+(setopt comment-style 'extra-line)
 
-  (setopt comment-empty-lines t)
-  (setopt comment-style 'extra-line)
+(setopt confirm-kill-emacs 'y-or-n-p)
 
-  (setopt confirm-kill-emacs 'y-or-n-p)
+;;;
+;;; restore useful but hidden commands
+;;;
 
-  (setopt ns-auto-hide-menu-bar t)           ; this gains no space on displays with the notch
-
-  (setopt standard-indent 8)
+(put 'scroll-left 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-defun 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 
+
 ;;;
-;;; the obviously needed packages.
-;;;
-
-  (use-package diminish)
-
-  (use-package bind-key)
-
-  (use-package free-keys)
-
-  (use-package which-key
-    :diminish
-    :init (which-key-mode))
-
-  (use-package ws-butler
-    :diminish
-    :hook (prog-mode . ws-butler-mode))
-
-  (use-package savehist
-    :config
-    (setopt savehist-additional-variables
-            '(compile-command
-              kill-ring
-              regexp-search-ring))
-    (savehist-mode 1))
-
-  (use-package saveplace
-    :config
-    (save-place-mode)
-    :custom
-    (save-place-limit 1000))
-
-  (use-package autorevert
-    :config
-    (global-auto-revert-mode +1)
-    :custom
-    (auto-revert-avoid-polling t)
-    (global-auto-revert-non-file-buffers t)
-    (auto-revert-verbose nil))
-
-  ;; for dired, use 'gls' if it's available. the 'ls' in macos and some
-  ;; other systems doesn't support all the options that 'dired' wants.
-
-  (use-package dired
-    :after exec-path-from-shell
-    :ensure nil
-    :config
-    (setopt
-     dired-recursive-copies  'always
-     dired-isearch-filenames 'dwim)
-    (when (executable-find "gls")    ; use GNU ls
-      (setopt dired-use-ls-dired nil)
-      (setopt ls-lisp-use-insert-directory-program t)
-      (setopt insert-directory-program "gls")
-      (setopt dired-listing-switches "-alh --group-directories-first")
-      (setopt dired-kill-when-opening-new-dired-buffer t)
-      (setopt dired-auto-revert-buffer t)
-      (setopt dired-do-revert-buffer t)))
-
-  ;; documentation with 'info' and 'eldoc'. for some reason i'm missing
-  ;; system info from homebrew. i should probably move this into my
-  ;; zshenv.
-
-  (use-package info
-    :after exec-path-from-shell
-    :custom
-    (Info-additional-directory-list '("/opt/homebrew/share/info")))
-
-  (use-package eldoc
-    :diminish
-    :config (global-eldoc-mode))
-
-  ;; i often use C-l for visual breaks.
-
-  (use-package form-feed-st
-    :diminish
-    :hook (prog-mode . form-feed-st-mode) (text-mode . form-feed-st-mode))
-
-
-;;;
-;;; themes fonts icons
+;;; global (re)keybinds
 ;;;
 
-  (use-package tomorrow-night-deepblue-theme
-    :pin melpa
-    :config
-    (mapc #'disable-theme custom-enabled-themes)
-    (load-theme 'tomorrow-night-deepblue t))
+;; this makes TAB in the minibuffer behave more like it does in a
+;; shell.
 
-  (use-package nerd-icons)
+(keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete)
 
-  (use-package nerd-icons-dired
-    :after nerd-icons
-    :diminish
-    :hook
-    (dired-mode . nerd-icons-dired-mode))
+;; use s-q to close emacs if i don't want to M-x save-buffers-kill-emacs.
 
-  (use-package nerd-icons-completion
-    :after nerd-icons
-    :diminish
-    :config
-    (declare-function nerd-icons-completion-mode "nerd-icons-completion")
-    (nerd-icons-completion-mode))
-  (declare-function nerd-icons-completion-marginalia-setup "nerd-icons-completion")
-  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+(global-unset-key (kbd "C-x C-c"))
 
-(use-package nerd-icons-ibuffer
-  :after nerd-icons
+;; the number of times i want a the list instead of the mode is zero.
+
+(global-set-key (kbd "C-x C-d") 'dired)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; default search to regexp instead of string.
+
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+
+;; movementish things
+
+;; ace-window should handle this now
+;; (global-set-key (kbd "M-o") 'other-window)
+
+;; to not through!
+
+(global-set-key "\M-z" 'zap-up-to-char)
+
+;; 'dot-mode' brings the vim '.' to emacs. this has a keybind of
+;; C-M-. which conflicts with xref-find-apropos.
+
+(use-package dot-mode
   :diminish
-  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+  :init
+  (declare-function global-dot-mode "dot-mode")
+  (global-dot-mode t))
+
+;; jumping
+
+(use-package avy
+  :demand t
+  :bind (("C-c j" . avy-goto-line)
+         ("s-j"   . avy-goto-char-timer)))
+
+(use-package ace-window
+  :after avy
+  :bind (("C-x o" . ace-window)
+	 ("M-o" . ace-window)))
+
 
+
+;;;
+;;; the small and obvious packages that don't need much configuration
+;;; beyond being included.
+;;;
+
+(use-package diminish)
+
+(use-package bind-key)
+
+(use-package free-keys)
+
+(use-package which-key
+  :diminish
+  :init (which-key-mode))
+
+(use-package ws-butler
+  :diminish
+  :hook (prog-mode . ws-butler-mode))
+
+(use-package savehist
+  :config
+  (setopt savehist-additional-variables
+          '(compile-command
+            kill-ring
+            regexp-search-ring))
+  (savehist-mode 1))
+
+(use-package saveplace
+  :config
+  (save-place-mode)
+  :custom
+  (save-place-limit 1000))
+
+(use-package autorevert
+  :config
+  (global-auto-revert-mode +1)
+  :custom
+  (auto-revert-avoid-polling t)
+  (global-auto-revert-non-file-buffers t)
+  (auto-revert-verbose nil))
+
+;; i often use C-l for visual breaks.
+
+(use-package form-feed-st
+  :diminish
+  :hook (prog-mode . form-feed-st-mode) (text-mode . form-feed-st-mode))
+
+
+
+;;;
+;;; built ins that need more configuration
+;;;
+
+;; for dired, use 'gls' if it's available. the 'ls' in macos and some
+;; other systems doesn't support all the options that 'dired' wants.
+
+(use-package dired
+  :after exec-path-from-shell
+  :ensure nil
+  :config
+  (setopt
+   dired-recursive-copies  'always
+   dired-isearch-filenames 'dwim)
+  (when (executable-find "gls")    ; use GNU ls
+    (setopt dired-use-ls-dired nil)
+    (setopt ls-lisp-use-insert-directory-program t)
+    (setopt insert-directory-program "gls")
+    (setopt dired-listing-switches "-alh --group-directories-first")
+    (setopt dired-kill-when-opening-new-dired-buffer t)
+    (setopt dired-auto-revert-buffer t)
+    (setopt dired-do-revert-buffer t)))
+
+;; documentation with 'info' and 'eldoc'. for some reason i'm missing
+;; system info from homebrew. i should probably move this into my
+;; zshenv.
+
+(use-package info
+  :after exec-path-from-shell
+  :custom
+  (Info-additional-directory-list '("/opt/homebrew/share/info")))
+
+(use-package eldoc
+  :diminish
+  :config (global-eldoc-mode))
+
+
+
+;;;
+;;; oddities with more complex configuration
+;;;
+
 
 ;; 'so-long' handles long lines that are usually found in program
 ;; source code where unneeded whitespace has been removed. forcing
@@ -311,6 +351,7 @@
   (bidi-paragraph-direction 'left-to-right))
 
 ;; 'imenu-list' puts 'imenu' in a sidebar.
+
 (use-package imenu-list
   :diminish
   :bind ("C-'" . imenu-list-smart-toggle)
@@ -318,46 +359,87 @@
   (imenu-list-focus-after-activation t)
   (imenu-list-auto-resize t))
 
-;; 'side-notes' lets you have notes files in any project or directory.
-;; the notes are opened in a side window like 'imenu-list'. searching
-;; for the notes files are done backward up the the directory path
-;; until one is found.
-
-(use-package side-notes
-  :diminish
-  :bind ("M-s n" . side-notes-toggle-notes)
-  :custom
-  (side-notes-file "side-notes.txt")
-  (side-notes-secondary-file "~/general-side-notes.txt"))
-
-;; 'dot-mode' brings the vim '.' to emacs.
-;; i may want to customize the keybinds. C-M-. conflicts with
-;; xref-find-apropos.
-
-(use-package dot-mode
-  :diminish
-  :init
-  (declare-function global-dot-mode "dot-mode")
-  (global-dot-mode t))
-
-;; moving at the speed of 'what was that keybind again?'.
-
-(use-package avy
-  :demand t
-  :bind (("C-c j" . avy-goto-line)
-         ("s-j"   . avy-goto-char-timer)))
-
-(use-package ace-window
-  :after avy
-  :bind (("C-x o" . ace-window)
-	 ("M-o" . ace-window)))
-
-
 ;; 'dumb-jump' is a dwim for 'xref'.
 
 (use-package dumb-jump
   :hook
   (xref-backend-functions . dump-jump-xref-activate))
+
+
+
+;;;
+;;; completion
+;;;
+
+
+
+;;;
+;;; simple configuration programming modes and supporting packages
+;;;
+
+;; magit, i can't escape it forever
+
+(use-package magit)
+
+(use-package cmake-mode)
+
+(use-package ninja-mode)
+
+(use-package ruby-ts-mode
+  :mode "\\.rb\\'"
+  :mode "Rakefile\\'"
+  :mode "Gemfile\\'")
+
+
+
+;;;
+;;; larger programming mode packages and settings
+;;;
+
+;; eglot, treesitter, flymake, &c., go here.
+
+
+
+;;;
+;;; themes fonts icons
+;;;
+
+(use-package tomorrow-night-deepblue-theme
+  :pin melpa
+  :config
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme 'tomorrow-night-deepblue t))
+
+(use-package nerd-icons)
+
+(use-package nerd-icons-dired
+  :after nerd-icons
+  :diminish
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package nerd-icons-completion
+  :after nerd-icons
+  :diminish
+  :config
+  (declare-function nerd-icons-completion-mode "nerd-icons-completion")
+  (nerd-icons-completion-mode))
+;; (declare-function nerd-icons-completion-marginalia-setup "nerd-icons-completion")
+;; (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-ibuffer
+  :after nerd-icons
+  :diminish
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+
+
+;;;
+;;; applications
+;;;
+
+;;;
+;;; minimalism resisting the weight of org-mode
+;;;
 
 ;; i know the intelligentsia all use 'org', but i don't. 'org' invites
 ;; too much fiddling. 'deft' provides a lightweight solution that
@@ -372,6 +454,19 @@
   (setopt deft-extension
           (assoc-default deft-text-mode '((markdown-mode . "md") (rst-mode . "rst"))
 			 'eq "txt")))
+
+;; 'side-notes' lets you have notes files in any project or directory.
+;; the notes are opened in a side window like 'imenu-list'. searching
+;; for the notes files are done backward up the the directory path
+;; until one is found.
+
+(use-package side-notes
+  :diminish
+  :bind ("M-s n" . side-notes-toggle-notes)
+  :custom
+  (side-notes-file "side-notes.txt")
+  (side-notes-secondary-file "~/general-side-notes.txt"))
+
 
 ;;;
 ;;; functions
@@ -424,30 +519,14 @@ With PREFIX, move the line containing point to line PREFIX of the window."
 	(delete-window window)))))
 
 (bind-key "C-x 5t" #'troi/tear-off-window)
+
 
+
 ;;;
-;;; programming mode common configuration and helpers
+;;; os related customization
 ;;;
 
-;; magit, i can't escape it forever
 
-(use-package magit
-  :ensure t
-  :bind (("C-x g" . magit-status)))
-
-(use-package cmake-mode
-  :defer t)
-
-(use-package ninja-mode
-  :defer t)
-
-(use-package ruby-ts-mode
-  :defer t
-  :after treesit
-  :mode "\\.rb\\'"
-  :mode "Rakefile\\'"
-  :mode "Gemfile\\'")
-
 ;;;
 ;;; touchpad touchiness
 ;;;
@@ -480,6 +559,7 @@ With PREFIX, move the line containing point to line PREFIX of the window."
    (mouse-wheel-mode -1)
    (message "trackpad stuff set to ignore")))
 
+
 ;; i keep brushing and confusing my touchpad. this moves the mouse out
 ;; of the way. i move it up from the lower right corner to avoid
 ;; pulling up the mac start bar.
@@ -488,6 +568,7 @@ With PREFIX, move the line containing point to line PREFIX of the window."
 (setopt mouse-avoidance-banish-position
 	'((frame-or-window . frame) (side . right) (side-pos . 1)
 	  (top-or-bottom . bottom) (top-or-bottom-pos . 10)))
+
 
 ;; mac keyboards and keybinds:
 ;;
@@ -506,7 +587,9 @@ With PREFIX, move the line containing point to line PREFIX of the window."
 ;; spacebar -> unchanged
 ;; command  -> unchanged
 ;; option   -> unchanged
+
 
+
 ;;;
 ;;; easy customization interface
 ;;;
