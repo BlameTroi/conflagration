9dd3efddfa9d981199d40d235218af4a1a1c80e4
troi <blametroi@gmail.com>
2024-12-20 08:47:45 -0500

    cleaner custom

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -11,22 +11,26 @@
  '(global-disable-mouse-mode-lighter "")
  '(js-indent-level 8)
  '(package-selected-packages
-   '(ace-window astyle auto-hide cape cmake-mode cobol-mode
-		corfu-prescient corfu-terminal deft diff-hl diminish
-		disable-mouse dot-mode dumb-jump eat eglot-java
-		exec-path-from-shell flymake-guile form-feed-st
-		free-keys git-modes gitignore-snippets hide-mode-line
-		hl-todo imenu-list inf-ruby java-snippets json-mode
-		kind-icon license-snippets magit-file-icons marginalia
-		markdown-ts-mode nerd-icons-completion
-		nerd-icons-corfu nerd-icons-dired nerd-icons-ibuffer
+   '(acme-theme astyle auto-hide auto-highlight-symbol cape cmake-mode
+		cobol-mode comment-tags corfu-prescient corfu-terminal
+		deft diff-hl diminish disable-mouse dot-mode dumb-jump
+		eat ef-themes eglot-java exec-path-from-shell
+		flymake-guile form-feed-st free-keys git-modes
+		gitignore-snippets hide-mode-line
+		highlight-parentheses hl-todo imenu-list inf-ruby
+		java-snippets json-mode kind-icon license-snippets
+		magit-file-icons marginalia markdown-ts-mode
+		nerd-icons-completion nerd-icons-corfu
+		nerd-icons-dired nerd-icons-ibuffer
 		nerd-icons-ivy-rich ninja-mode no-littering ob-sml
-		ob-sql-mode ob-typescript orderless org
-		page-break-lines sicp side-notes smartparens
-		tomorrow-night-deepblue-theme trashed treesit-auto
-		vertico-prescient which-key ws-butler yaml-mode
-		yasnippet-capf yasnippet-classic-snippets
-		yasnippet-snippets yatemplate ztree))
+		ob-sql-mode ob-typescript orderless org-autolist
+		org-bullets org-modern page-break-lines paredit
+		rainbow-delimiters sicp side-notes smartparens
+		spacious-padding tomorrow-night-deepblue-theme trashed
+		treemacs treesit-auto vertico-prescient which-key
+		ws-butler yaml-mode yasnippet-capf
+		yasnippet-classic-snippets yasnippet-snippets
+		yatemplate ztree))
  '(package-vc-selected-packages
    '((auto-hide :url "https://github.com/BlameTroi/auto-hide.el" :branch
 		"main")))
@@ -38,5 +42,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(completion-preview-highlight ((t (:inherit highlight :background "dark gray"))))
- '(icomplete-selected-match ((t (:inherit highlight :background "dark gray")))))
+ )
