0be470ed0288bee44f1858fe69f2d156cdc6a1bf
troi <blametroi@gmail.com>
2024-12-27 08:01:19 -0500

    going back

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -10,27 +10,7 @@
  '(global-disable-mouse-mode t)
  '(global-disable-mouse-mode-lighter "")
  '(js-indent-level 8)
- '(package-selected-packages
-   '(acme-theme astyle auto-hide auto-highlight-symbol cape cmake-mode
-		cobol-mode comment-tags corfu-prescient corfu-terminal
-		deft diff-hl diminish disable-mouse dot-mode dumb-jump
-		eat ef-themes eglot-java exec-path-from-shell
-		flymake-guile form-feed-st free-keys git-modes
-		gitignore-snippets hide-mode-line
-		highlight-parentheses hl-todo imenu-list inf-ruby
-		java-snippets json-mode kind-icon license-snippets
-		magit-file-icons marginalia markdown-ts-mode
-		nerd-icons-completion nerd-icons-corfu
-		nerd-icons-dired nerd-icons-ibuffer
-		nerd-icons-ivy-rich ninja-mode no-littering ob-sml
-		ob-sql-mode ob-typescript orderless org-autolist
-		org-bullets org-modern page-break-lines paredit
-		rainbow-delimiters sicp side-notes smartparens
-		spacious-padding tomorrow-night-deepblue-theme trashed
-		treemacs treesit-auto vertico-prescient which-key
-		ws-butler yaml-mode yasnippet-capf
-		yasnippet-classic-snippets yasnippet-snippets
-		yatemplate ztree))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((auto-hide :url "https://github.com/BlameTroi/auto-hide.el" :branch
 		"main")))
@@ -42,4 +22,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t :background "#00006f")))
+ '(header-line ((t :box (:line-width 4 :color "#00346e" :style nil))))
+ '(header-line-highlight ((t :box (:color "#ffffff"))))
+ '(keycast-key ((t)))
+ '(line-number ((t :background "#00006f")))
+ '(mode-line ((t :box (:line-width 6 :color "#00346e" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#00346e" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#ffffff"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#00346e" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#00006f" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#00346e" :style nil))))
+ '(tab-line-tab ((t)))
+ '(tab-line-tab-active ((t)))
+ '(tab-line-tab-inactive ((t)))
+ '(vertical-border ((t :background "#00006f" :foreground "#00006f")))
+ '(window-divider ((t (:background "#00006f" :foreground "#00006f"))))
+ '(window-divider-first-pixel ((t (:background "#00006f" :foreground "#00006f"))))
+ '(window-divider-last-pixel ((t (:background "#00006f" :foreground "#00006f")))))
