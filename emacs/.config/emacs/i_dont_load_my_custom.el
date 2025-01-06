28e43ac584333ddedfb8700e0724ae639bfd683c
troi <blametroi@gmail.com>
2025-01-06 11:59:08 -0500

    diminish extra compile-angel modes

--- emacs/.config/emacs/i_dont_load_my_custom.el
+++ emacs/.config/emacs/i_dont_load_my_custom.el
@@ -4,7 +4,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(acme-theme astyle auto-hide cape cmake-mode cobol-mode compile-angel
+		corfu-prescient deft diff-hl diminish dumb-jump
+		exec-path-from-shell form-feed-st geiser-chez
+		git-modes hide-mode-line kind-icon marginalia
+		nerd-icons-completion nerd-icons-corfu
+		nerd-icons-dired nerd-icons-ibuffer ninja-mode
+		no-littering ob-sml ob-typescript org-bullets
+		org-modern side-notes sml-basis smlfmt
+		treemacs-nerd-icons treesit-auto vertico
+		visual-fill-column wgrep ws-butler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
