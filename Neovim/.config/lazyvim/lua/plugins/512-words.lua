fbb698963ba2e769850ddcd20c83640e2bddbc93
troi <blametroi@gmail.com>
2025-05-05 09:04:08 -0400

    kitty, 512 words changes, new theme--lazyvim has unsynched changes

--- Neovim/.config/lazyvim/lua/plugins/512-words.lua
+++ Neovim/.config/lazyvim/lua/plugins/512-words.lua
@@ -1,15 +1,16 @@
-local M = {
-  "BlameTroi/512-words",
+return {
+  --  "BlameTroi/512-words",
+  dir = "~/Projects/Neovim/512-words",
   vim.keymap.set("n", "gW", function()
     require("512-words").open()
   end),
   opts = {
     buffer = {
-      textwidth = 80,
+      textwidth = 75,
     },
     words = 0x100,
     storage_directory = "~/Notepad",
+    date_prefix = "#",
     file_extension = ".md",
   },
 }
-return M
